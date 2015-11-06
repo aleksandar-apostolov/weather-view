6aed6b40144468d47017af46b2bddac4e7db9aa7
aapostol <aleksandar.apostolov@netcetera.com>
2015-11-07 00:08:42 +0100

    - add gif drawable - add gson - add initial weather service - ignore local.properties

---
+++ weather-view-lib/src/main/java/com/aapii/codecamp/weatherview/service/WeatherService.java
@@ -0,0 +1,20 @@
+package com.aapii.codecamp.weatherview.service;
+
+import android.app.AppOpsManager;
+
+/**
+ * Created by aapostol on 06.11.2015.
+ */
+public class WeatherService {
+  private static final String LOG_TAG = "WeatherService";
+
+  private static final String API_KEY = "3ac4c8e628d0cb6cedd2ded241cd73b1";
+  private static final String API_URL_SUFFIX = "APPID=" + API_KEY;
+  private static final String API_URL = "";
+
+  protected void configure() {
+
+  }
+
+
+}
